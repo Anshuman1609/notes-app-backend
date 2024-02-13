6a06bb33c50ac8c0b15b8c60c85621729c196e3d
Anshuman Dubey <112381325+Anshuman1609@users.noreply.github.com>
2024-02-13 12:40:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const connection = mongoose.connect(process.env.mongoUrl);
 
-module.exports = {
-  connection,
-};
+// Set the strictQuery option to prepare for the deprecation
+mongoose.set("strictQuery", false);
+
+// MongoDB Atlas connection URL
+const mongoUrl = process.env.mongoUrl;
+
+// Connect to MongoDB Atlas cluster
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas cluster successfully.");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas cluster:", error.message);
+  });
+
+module.exports = mongoose.connection;
